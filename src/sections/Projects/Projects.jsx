6963d5d1a57e9c62b2e8a3a0fe282ca946823b05
Pound1 <50385700+Pound1@users.noreply.github.com>
2024-11-13 22:10:41 +1100

    Updated project space and added Project Card component

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,16 +1,32 @@
 import styles from './ProjectsStyles.module.css';
 import viberr from '../../assets/viberr.png';
+import hipsster from '../../assets/hipsster.png';
+import fitlift from '../../assets/fitlift.png';
+import ProjectCard from '../../common/ProjectCard';
 
 function Projects() {
   return (
     <section id="projects" className={styles.container}>
       <h1 className="sectionTitle">Projects</h1>
-      <div className={styles.projectContainer}>
-        <a href="https://google.com">
-          <img className="hover" src={viberr} alt="viberr image" />
-          <h3>Viberr</h3>
-          <p>Streaming Service</p>
-        </a>
+      <div className={styles.projectsContainer}>
+        <ProjectCard
+          src={viberr}
+          link="https://google.com"
+          h3="Viberr"
+          p="Streaming Service"
+        />
+        <ProjectCard
+          src={hipsster}
+          link="https://google.com"
+          h3="Hipsster"
+          p="Modern Optometrist"
+        />
+        <ProjectCard
+          src={fitlift}
+          link="https://google.com"
+          h3="Fitlift"
+          p="Fitness app"
+        />
       </div>
     </section>
   );
